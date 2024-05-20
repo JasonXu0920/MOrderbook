1d5b43c95d93cb1a40ba4a6836d545cf6ad2b3b0
Jason Xu <97411709+JasonXu0920@users.noreply.github.com>
2024-05-21 08:08:40 +1000

    orderbook level info constructor

--- Orderbook.cpp
+++ Orderbook.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <algorithm>
-#include <unordered_map>
-#include <variant>
-#include <option>
-#include <tuple>
-#include <format>
+#include <vector>
+#include <string>
+
+
+
 
 enum class OrderType
 {
@@ -18,7 +17,36 @@ enum class Side
     Sell
 };
 
-int main {
-    std::cout << "Hello World" << std::endl;
-    return 0;
+using Price = std::int32_t;
+using Quantity = std::uint32_t;
+using OrderId = std::uint64_t;
+
+struct LevelInfo
+{
+	Price price_;
+	Quantity quantity_;
+};
+
+using LevelInfos = std::vector<LevelInfo>;
+
+class OrderbookLevelInfos
+{
+public:
+	OrderbookLevelInfo(const LevelInfos& bids, const LevelInfos& asks)
+		: bids_{ bids }
+		, asks_{ asks }
+	{ }
+	
+	const LevelInfos& GetBids() const { return bids_; }
+	const LevelInfos& GetAsks() const { return asks_; }
+
+private:
+	LevelInfos bids_;
+	LevelInfos asks_;
+};
+
+
+int main()
+{
+	return 0;
 }
