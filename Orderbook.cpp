26f8c7773e31d8d75fb1d2ffe32e4839565a141f
Jason Xu <97411709+JasonXu0920@users.noreply.github.com>
2024-05-24 08:05:32 +1000

    trade class

--- Orderbook.cpp
+++ Orderbook.cpp
@@ -113,6 +113,31 @@ private:
 	Quantity quantity_;
 };
 
+struct TradeInfo
+{
+	OrderId orderId_;
+	Price price_;
+	Quantity quantity_;
+};
+
+class Trade
+{
+public:
+	Trade(const TradeInfo& bidTrade, const TradeInfo& askTrade)
+		: bidTrade_ {bidTrade}
+		, askTrade_ {askTrade}
+	{ }
+
+	const TradeInfo& GetBidTrade() const { return bidTrade_; }
+	const TradeInfo& GetAskTrade() const { return askTrade_; }
+
+private:
+	TradeInfo bidTrade_;
+	TradeInfo askTrade_;
+};
+
+using Trades = std::vector<Trade>;
+
 int main()
 {
 	return 0;
