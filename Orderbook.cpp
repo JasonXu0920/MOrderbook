56908fb3914b9805373595097e4627177a022532
Jason Xu <97411709+JasonXu0920@users.noreply.github.com>
2024-05-27 08:11:11 +1000

    order matching

--- Orderbook.cpp
+++ Orderbook.cpp
@@ -65,6 +65,7 @@ public:
 	Quantity GetInitialQuantity() const { return initialQuantity_;}
 	Quantity GetRemainingQuantity() const { return remainingQuantity_;}
 	Quantity GetFilledQuantity() const { return GetInitialQuantity() - GetRemainingQuantity(); }
+	bool IsFilled() const { return GetRemainingQuantity()==0; }
 	void Fill(Quantity quantity)
 	{
 		if (quantity > GetRemainingQuantity())
@@ -151,8 +152,69 @@ private:
 	std::map<Price, OrderPointers, std::less<Price>> asks_;
 	std::unordered_map<OrderId, OrderEntry> orders_;
 	
-	
-}
+	bool CanMatch(Side side, Price price) const
+	{
+		if (side == Side::Buy)
+		{
+			if (asks_.empty)
+				return false;
+			
+			const auto& [bestAsk, _] = *asks_.begin();
+			return price >= bestAsk;
+		}
+		else
+		{
+			if (bids_.empty())
+				return false;
+			
+			const auto& [bestBid, _] = *bids_.begin();
+			return price <= bestBid;
+		}
+	}
+
+	Trades MatchOrders()
+	{
+		Trades trades;
+		trades.reserve(orders_.size());
+
+		while( true)
+		{
+			if ( bids_.empty() || asks_.empty())
+				break;
+			
+			auto& [bidPrice, bids] =  *bids_.begin();
+			auto& [askPrice, asks] = *asks_.begin();
+
+			Quantity quantity = std::min(bid->GetRemainingQuantity(), ask->GetRemainingQuantity());
+
+			bid->Fill(quantity);
+			ask->Fill(quantity);
+
+			if (bid->IsFilled())
+			{
+				bids.pop_front();
+				orders_.erase(bid->GetOrderId());
+			}
+
+			if (ask->IsFilled())
+			{
+				asks.pop_front();
+				orders_.erase(ask->GetOrderId());
+			}
+
+			if( bids.empty())
+				bids_.erase(bidPrice);
+			
+			if( asks.empty())
+				asks_.erase(askPrice);
+			
+			trades.push_back(Trade{
+				TradeInfo{ bid->GetOrderId(), bid->GetPrice(), quantity },
+				TradeInfo{ ask->GetOrderId(), ask->GetPrice(), quantity }
+			});
+		}
+	}
+};
 
 int main()
 {
